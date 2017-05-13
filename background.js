65e3973aad95830081bdcc88ffc2a7040dc0cc50
Markus Ziller <mail.markus.ziller@gmail.com>
2017-05-13 22:28:14 +0200

    Refactored content.js and background.js and added rudimentary workflow management

--- background.js
+++ background.js
@@ -1,23 +1,45 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    console.log(tabs[0].id, '!!!sdfsfsdf');
     chrome.tabs.executeScript(null, { file: "content.js" }, () => {
       chrome.tabs.executeScript(null, { file: "libs/jquery-3.2.1.min.js" }, () => {
+        jQuery.get(chrome.extension.getURL('html/overlay01.html'), (htmlTemplate) => {
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', chrome.extension.getURL('html/overlay01.html'), true);
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            const div = xhr.responseText;
-            chrome.tabs.sendMessage(tabs[0].id, { data: 'test', event: 'clickInital', template: div }, function(response) {
-              chrome.tabs.captureVisibleTab(function(imageBinary) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            event: 'getState'
+          }, (response) => {
+            if (response.paused === true) {
+              chrome.tabs.sendMessage(tabs[0].id, {
+                event: 'continueVideo'
+              }, (response) => {
+                console.log(response.status === 'ok' ? 'continued video' : 'error');
               });
-              console.log('success', response);
-            });
-          }
-        };
-        xhr.send();
+            } else {
+              chrome.tabs.sendMessage(tabs[0].id, {
+                event: 'clearView'
+              }, (response) => {
+                if (response.status === 'ok') {
+                  chrome.tabs.captureVisibleTab(function(imageBinary) {
+                    jQuery.post(
+                      "http://10.100.126.230:3000/images",
+                      {
+                        base64Image: imageBinary
+                      },
+                      function(data) {
+                        console.log('took screenshot', data.url);
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                          event: 'showViewFainderOverlay',
+                          template: htmlTemplate
+                        }, (response2) => {
+                          console.log('fainder overlay added');
 
+                        });
+                      });
+                  });
+                }
+              });
+            }
+          });
+        });
       });
     });
   });
--- content.js
+++ content.js
@@ -1,31 +1,67 @@
 let listenerAdded = false;
 
 let listener2 = function(msg, sender, callback) {
-  if (msg.event === 'clickInital') {
+  if(msg.event === 'getState') {
+    const isPaused = jQuery('#player-controls--play-toggle').attr('class') === 'play-pause-toggle player-control play';
+    return callback({ paused: isPaused });
+  }
+
+  if(msg.event === 'continueVideo') {
+    jQuery('#playerManualOverlay').remove();
+    jQuery('#player-controls--back-button').show();
+    jQuery('#player-controls--display-details').show();
+    jQuery('#player-controls--settings-control').show();
+    jQuery('#player-controls-playback-controls').show();
+    jQuery('#player-controls--play-toggle').click();
+    return callback({ status: 'ok' });
+  }
+
+  if(msg.event === 'clearView') {
+    jQuery('#playerManualOverlay').remove();
+    jQuery('#player-controls--back-button').hide();
+    jQuery('#player-controls--display-details').hide();
+    jQuery('#player-controls--settings-control').hide();
+    jQuery('#player-controls-playback-controls').hide();
+    jQuery('#player-controls--play-toggle').click();
+    return callback({ status: 'ok' });
+  }
+
+  if(msg.event === 'showViewFainderOverlay') {
+    jQuery('#player-controls--back-button').hide();
+    jQuery('#player-controls--display-details').hide();
+    jQuery('#player-controls--settings-control').hide();
+    jQuery('#player-controls-playback-controls').hide();
+
+    const e = jQuery(msg.template);
+    jQuery('#playerControls').append(e);
+
+    e.attr('id', 'playerManualOverlay');
+    return callback({ status: 'ok' });
+  }
+
+  if (msg.event === 'clickInitial') {
     console.log('called at ' + Date.now(), msg, sender);
     jQuery('#player-controls--play-toggle').click();
 
-    jQuery('#player-controls--back-button').toggle();
-    jQuery('#player-controls--display-details').toggle();
-    jQuery('#player-controls--settings-control').toggle();
-    jQuery('#player-controls-playback-controls').toggle();
-    // jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://raw.githubusercontent.com/7hack-vod-commerce/chrome-plugin/master/css/viewfainder.css">');
+    const isPaused = jQuery('#player-controls--play-toggle').attr('class') === 'play-pause-toggle player-control play';
 
+    if(!isPaused) {
+      jQuery('#playerManualOverlay').remove();
+      jQuery('#player-controls--back-button').show();
+      jQuery('#player-controls--display-details').show();
+      jQuery('#player-controls--settings-control').show();
+      jQuery('#player-controls-playback-controls').show();
+    } else {
+      jQuery('#player-controls--back-button').hide();
+      jQuery('#player-controls--display-details').hide();
+      jQuery('#player-controls--settings-control').hide();
+      jQuery('#player-controls-playback-controls').hide();
 
-    jQuery('#playerManualOverlay').remove();
+      const e = jQuery(msg.template);
+      jQuery('#playerControls').append(e);
 
-    // const arr = [1, 2, 3];
-    // const divs = arr.map(a => '<div style="z-index: 100000 !important; height: 300px">' + a + '<div>');
-    // let div = '<div style="z-index: 100000 !important; height: 300px">called at' + divs + '<div>';
-    // jQuery('#playerControls').append(msg.template);
-
-    var e = jQuery(msg.template);
-    jQuery('#playerControls')
-      .append(e);
-
-
-    e.attr('id', 'playerManualOverlay');
-
+      e.attr('id', 'playerManualOverlay');
+    }
 
     callback({ data: 'ok', success: true });
   }
